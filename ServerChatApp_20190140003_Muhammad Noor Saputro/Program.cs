314d14f343a8c584951985e7045d50359b14238f
Saputro-bit <muhammadnoorsaputro23@gmail.com>
2022-01-10 17:56:16 +0700

    Add project files.

---
+++ ServerChatApp_20190140003_Muhammad Noor Saputro/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using WcfService_2Way_003;
+
+namespace ServerChatApp_20190140003_Muhammad_Noor_Saputro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+            try
+            {
+                hostObjek = new ServiceHost(typeof(ServiceCallback));
+                hostObjek.Open();
+
+                Console.WriteLine("Server On, Ready to use");
+                Console.ReadLine();
+
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
